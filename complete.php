5fb5be1e520f96f5f96fb7a82884e7231df8d69a
carlosThatzad <carlos.albala@thatzad.com>
2020-04-30 19:20:41 +0200

    [Modificacion del archivo complet.php]

--- complete.php
+++ complete.php
@@ -2,6 +2,8 @@
 	session_start();
 	include 'lib/con.php';
 
+	echo "Esto es una modificacion";
+
 	//preparing statement
 	$stmt=$conn->prepare("UPDATE tasks SET completed=1 WHERE user=? AND id=?");
 	$stmt->bind_param("ii",$_SESSION['id'],$_GET['task']);
@@ -9,3 +11,4 @@
 	$stmt->close();
 	header('Location:list.php');
 	exit;
+
